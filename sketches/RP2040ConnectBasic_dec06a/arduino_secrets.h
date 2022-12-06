4b8661f254e140267a762c600d7917a8dea63833
Peter Bendel <peter@familie-bendel.de>
2022-12-06 18:29:39 +0100

    RP2040 Connect sketches

---
+++ sketches/RP2040ConnectBasic_dec06a/arduino_secrets.h
@@ -0,0 +1,2 @@
+#define SECRET_SSID "Vhs2g" // hier steht die SSID (Netzwerkkennung) deines Gäste WLANS
+#define SECRET_OPTIONAL_PASS "das ist geheim und gehört nicht ins github" // hier steht das Passwort für dein WLAN
